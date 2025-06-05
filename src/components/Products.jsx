d41660d173c17e09412adaa84bce16d400049410
kkennethsieu <kkennethsieu@gmail.com>
2025-06-05 16:58:36 -0700

    fixed mobile framer motion

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -18,10 +18,10 @@ function Products({ title }) {
         {title}
       </h3>
       <motion.div
-        initial={{ opacity: 0, x: -150 }}
+        initial={{ opacity: 0, x: -100 }}
         whileInView={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.8, ease: "easeOut", delay: 0.2 }}
-        viewport={{ once: true, amount: 0.3 }}
+        viewport={{ once: true, amount: 0.2 }}
       >
         <div className="grid grid-cols-1 gap-8 mt-10 sm:grid-cols-2 lg:grid-cols-3 place-items-center">
           {products.map((product) => (
